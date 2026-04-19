d5b435f94a54e8c26e5b583ba152e09dc5591ccc
Muhammad Usman <maanlive398@gmail.com>
2026-04-19 16:13:27 +0500

    solve corse error

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:3000"],
+  origin: process.env.Frontend_URL || "http://localhost:3000",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
