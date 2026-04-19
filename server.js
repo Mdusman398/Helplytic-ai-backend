ba86865ff61dfab31503599d01405bf8ed2cebf3
Muhammad Usman <maanlive398@gmail.com>
2026-04-19 16:16:09 +0500

    cors issue

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: process.env.Frontend_URL || "http://localhost:3000",
+  origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
